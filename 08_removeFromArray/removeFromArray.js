9e38d637c24a2ecb3a8d9cc6ad3a78b2aebfbdd5
Fellryn <163130057+Fellryn@users.noreply.github.com>
2025-10-10 19:15:43 +1000

    Finish 08_removeFromArray

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,5 +1,17 @@
-const removeFromArray = function() {
-};
+const removeFromArray = function (arr) {
+    if (arguments.length == 1) return arr;
+    newArr = [...arr];
 
+    for (let i = 1; i < arguments.length; i++) {
+        for (let j = 0; j < arr.length; j++) {
+            let index = newArr.indexOf(arguments[i]);
+            if (index != -1) {
+                newArr.splice(index, 1);
+                j--;
+            }
+        }
+    }
+    return newArr;
+};
 // Do not edit below this line
 module.exports = removeFromArray;
