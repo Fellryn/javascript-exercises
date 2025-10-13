cbb63383bdd72dcfba5798a027e7c34da3f0a933
Fellryn <163130057+Fellryn@users.noreply.github.com>
2025-10-13 11:52:12 +1000

    Finish 13_palindromes

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,16 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    const lowerCase = string.toLowerCase();
+    const alphanumerical = "abcdefghijklmnopqrstuvwxyz0123456789";
+    let strAsArr = [...lowerCase];
+    strAsArr = strAsArr.filter((value) => alphanumerical.includes(value));
+    console.log(strAsArr);
 
+    for(let i = 0; i < strAsArr.length / 2; i++)
+    {
+        if (strAsArr[i] != strAsArr[strAsArr.length - i - 1])
+            return false;
+    }
+    return true;
 };
 
 // Do not edit below this line
