4c74f6fca4ea7cd4318222ff5e894b2c82312b56
Fellryn <163130057+Fellryn@users.noreply.github.com>
2025-10-13 11:26:04 +1000

    Finish 12_calculator

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,34 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  return array.reduce((total, item) => {
+    return total + item;
+  }, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce((total, item) => {
+    return total *= item;
+  }, 1);
 };
 
-const power = function() {
-	
+const power = function(a, e) {
+  return a ** e;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+	let res = 1;
+  for (let i = 1; i <= a; i++)
+  {
+    res *= i;
+  }
+  return res;
 };
 
 // Do not edit below this line
