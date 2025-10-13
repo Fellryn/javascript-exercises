732d5349729224326740bd6e6a6d6560060c906e
Fellryn <163130057+Fellryn@users.noreply.github.com>
2025-10-13 12:45:59 +1000

    Finish 16_findTheOldest

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,18 @@
-const findTheOldest = function() {
+const getAge = function(birth, death) {
+    if (!death) {
+        return new Date().getFullYear() - birth;
+    } 
+    else {
+        return death - birth;
+    }
+}
 
+const findTheOldest = function(people) {
+   return people.reduce((oldest, currentPerson) => {
+        const oldestAge = getAge(oldest.yearOfBirth, oldest.yearOfDeath);
+        const currentAge = getAge(currentPerson.yearOfBirth, currentPerson.yearOfDeath);
+        return oldestAge < currentAge ? currentPerson : oldest;
+    });
 };
 
 // Do not edit below this line
