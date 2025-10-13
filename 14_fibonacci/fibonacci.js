a347274c7c87c5261069478e8e6d9bb9e6da44b6
Fellryn <163130057+Fellryn@users.noreply.github.com>
2025-10-13 12:19:01 +1000

    Finish 14_fibonacci

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,11 @@
-const fibonacci = function() {
-
+const fibonacci = function(index) {
+    if (+index == 0) return 0;
+    if (index <= -1) return "OOPS";
+    let fib = [1, 1];
+    for (let i = 1; i < +index; i++) {
+        fib.push((fib[i - 1] + fib[i]));
+    }
+    return fib[fib.length - 2];
 };
 
 // Do not edit below this line
